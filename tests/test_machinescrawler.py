b88c0362011a48e5060481c45e1dc0c0bf41a844
Karl Marx <marxsouza@protonmail.com>
2022-03-24 11:12:52 -0300

    test: Updated project name

--- tests/test_machinescrawler.py
+++ tests/test_machinescrawler.py
@@ -1,9 +1,9 @@
 from unittest import TestCase
 
-from machinescrawlers import MachinesCrawler
+from machinescatchers import MachinesCatcher
 
 
 class MachinesCrawlerTest(TestCase):
     def test_get_unknown_platform_machines(self):
         with self.assertRaises(KeyError):
-            MachinesCrawler("foobar")
+            MachinesCatcher("foobar")
